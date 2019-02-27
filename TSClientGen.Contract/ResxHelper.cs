8a223174eaad997ddaf9562dbb9583cbb72c965e
Anatoliy Koperin <a.koperin@smartcat.ai>
2019-02-27 10:01:00 +0300

    code style fixes

--- TSClientGen.Contract/ResxHelper.cs
+++ TSClientGen.Contract/ResxHelper.cs
@@ -4,26 +4,26 @@ using System.Resources;
 
 namespace TSClientGen
 {
-    /// <summary>
-    /// Вспомогательный класс для упрощения работы с ресурсами
-    /// </summary>
-    internal static class ResxHelper
-    {
-        public static ResourceManager GetResourceManager(this Type resxType)
-        {
-            var resourceManagerProperty = resxType.GetProperty("ResourceManager", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+	/// <summary>
+	/// Вспомогательный класс для упрощения работы с ресурсами
+	/// </summary>
+	internal static class ResxHelper
+	{
+		public static ResourceManager GetResourceManager(this Type resxType)
+		{
+			var resourceManagerProperty = resxType.GetProperty("ResourceManager", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
 			
-            if (resourceManagerProperty == null)
-                throw new ArgumentException(
-                    $"Parameter must have static property ResourceManager (type {resxType.FullName})",
-                    nameof(resxType));
+			if (resourceManagerProperty == null)
+				throw new ArgumentException(
+					$"Parameter must have static property ResourceManager (type {resxType.FullName})",
+					nameof(resxType));
 			
-            if (resourceManagerProperty.PropertyType != typeof(ResourceManager))
-                throw new ArgumentException(
-                    "Static property ResourceManager has unexpected type (type {resxType.FullName})",
-                    nameof(resxType));
+			if (resourceManagerProperty.PropertyType != typeof(ResourceManager))
+				throw new ArgumentException(
+					"Static property ResourceManager has unexpected type (type {resxType.FullName})",
+					nameof(resxType));
 			
-            return (ResourceManager) resourceManagerProperty.GetValue(null);;
-        }
-    }
+			return (ResourceManager) resourceManagerProperty.GetValue(null);;
+		}
+	}
 }
--- TSClientGen.Contract/TSExposeResxAttribute.cs
+++ TSClientGen.Contract/TSExposeResxAttribute.cs
@@ -3,20 +3,20 @@ using System.Resources;
 
 namespace TSClientGen
 {
-    /// <summary>
-    /// Атрибут, с помощью которого можно указать ресурсный файл, который должен быть доступен клиенту
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Assembly)]
-    public class TSExposeResxAttribute : Attribute
-    {
-        public TSExposeResxAttribute(Type resxType)
-        {
-            ResxName = resxType.Name;
-            ResourceManager = resxType.GetResourceManager();
-        }
+	/// <summary>
+	/// Атрибут, с помощью которого можно указать ресурсный файл, который должен быть доступен клиенту
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Assembly)]
+	public class TSExposeResxAttribute : Attribute
+	{
+		public TSExposeResxAttribute(Type resxType)
+		{
+			ResxName = resxType.Name;
+			ResourceManager = resxType.GetResourceManager();
+		}
 
-        public ResourceManager ResourceManager { get; }
+		public ResourceManager ResourceManager { get; }
 
-        public string ResxName { get; }
-    }
+		public string ResxName { get; }
+	}
 }
