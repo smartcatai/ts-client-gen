99b602da90ac797315c0e16180612ca4f9bd4406
s.isaev <s.isaev@smartcat.ai>
2019-06-27 18:14:55 +0300

    Version update

--- TSClientGen/Properties/AssemblyInfo.cs
+++ TSClientGen/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("5.0.1")]
-[assembly: AssemblyInformationalVersion("5.0.1")]
+[assembly: AssemblyVersion("5.0.2")]
+[assembly: AssemblyInformationalVersion("5.0.2")]
 
 [assembly: AssemblyTitle("TSClientGen")]
 [assembly: AssemblyDescription("")]
