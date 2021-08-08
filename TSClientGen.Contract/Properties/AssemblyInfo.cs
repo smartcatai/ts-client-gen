d1af91c23f0d9d60c9366ae19f7b27d8fe95a88c
Vladimir Belyakov <v.belyakov@smartcat.ai>
2021-08-08 12:32:20 +0300

    CSMS-15000 Fix getUri function of axios transport.

--- TSClientGen.Contract/Properties/AssemblyInfo.cs
+++ TSClientGen.Contract/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("7.1.0")]
-[assembly: AssemblyInformationalVersion("7.1.0")]
+[assembly: AssemblyVersion("7.1.1")]
+[assembly: AssemblyInformationalVersion("7.1.1")]
 
 [assembly: AssemblyTitle("TSClientGen.Contract")]
 [assembly: AssemblyDescription("Contracts assembly with attributes for decorating api for TSClientGen tool")]
