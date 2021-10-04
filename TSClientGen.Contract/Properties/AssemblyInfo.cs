3d032e0331a5dfc92810b37f426b7a8df9012092
Vladimir Belyakov <v.belyakov@smartcat.ai>
2021-10-04 18:33:18 +0300

    CSMS-16400 Set axios default put header to application/json.

--- TSClientGen.Contract/Properties/AssemblyInfo.cs
+++ TSClientGen.Contract/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("7.1.2")]
-[assembly: AssemblyInformationalVersion("7.1.2")]
+[assembly: AssemblyVersion("7.1.3")]
+[assembly: AssemblyInformationalVersion("7.1.3")]
 
 [assembly: AssemblyTitle("TSClientGen.Contract")]
 [assembly: AssemblyDescription("Contracts assembly with attributes for decorating api for TSClientGen tool")]
