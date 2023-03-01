55a1ac6e7083d85fd2ade864ec3b1353f700c211
Alexander Kozhukhov <a.kozhuhov@smartcat.ai>
2023-03-01 17:12:17 +0300

    up version

--- TSClientGen.Contract/Properties/AssemblyInfo.cs
+++ TSClientGen.Contract/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("9.2.1")]
-[assembly: AssemblyInformationalVersion("9.2.1")]
+[assembly: AssemblyVersion("9.2.2")]
+[assembly: AssemblyInformationalVersion("9.2.2")]
 
 [assembly: AssemblyTitle("TSClientGen.Contract")]
 [assembly: AssemblyDescription("Contracts assembly with attributes for decorating api for TSClientGen tool")]
