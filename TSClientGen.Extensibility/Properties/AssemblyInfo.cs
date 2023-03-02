e94a917ee6ea4ce4dee6792b8ec554d1d47fffab
Alexander Kozhukhov <a.kozhuhov@smartcat.ai>
2023-03-02 13:29:34 +0300

    fix generation error

--- TSClientGen.Extensibility/Properties/AssemblyInfo.cs
+++ TSClientGen.Extensibility/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("9.2.3")]
-[assembly: AssemblyInformationalVersion("9.2.3")]
+[assembly: AssemblyVersion("9.2.4")]
+[assembly: AssemblyInformationalVersion("9.2.4")]
 
 [assembly: AssemblyTitle("TSClientGen.Extensibility")]
 [assembly: AssemblyDescription("Contracts assembly for authoring plugins for TSClientGen tool")]
