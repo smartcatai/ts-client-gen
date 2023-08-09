8387efbfb96ffa2437d8a81dd39416606f6b3c6d
VladimirBeliakov <34035524+VladimirBeliakov@users.noreply.github.com>
2023-08-09 17:10:43 +0400

    CSMS-36491 Add net 7 to TFW. (#40)
    Co-authored-by: Vladimir Belyakov <v.belyakov@smartcat.ai>

--- TSClientGen.Contract/Properties/AssemblyInfo.cs
+++ TSClientGen.Contract/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("9.2.4")]
-[assembly: AssemblyInformationalVersion("9.2.4")]
+[assembly: AssemblyVersion("9.2.5")]
+[assembly: AssemblyInformationalVersion("9.2.5")]
 
 [assembly: AssemblyTitle("TSClientGen.Contract")]
 [assembly: AssemblyDescription("Contracts assembly with attributes for decorating api for TSClientGen tool")]
