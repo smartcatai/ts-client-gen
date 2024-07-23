09344532e90ab06b0edb944e9d6797743ec389e4
Nikolay Zoteev <n.zoteev@smartcat.ai>
2024-07-23 19:35:10 +0400

    DEV-1380 Fix js-errors with baseURL & headers & removed TSSupportsExternalHostAttribute

--- TSClientGen.Contract/Properties/AssemblyInfo.cs
+++ TSClientGen.Contract/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("9.3.0")]
-[assembly: AssemblyInformationalVersion("9.3.0")]
+[assembly: AssemblyVersion("9.3.1")]
+[assembly: AssemblyInformationalVersion("9.3.1")]
 
 [assembly: AssemblyTitle("TSClientGen.Contract")]
 [assembly: AssemblyDescription("Contracts assembly with attributes for decorating api for TSClientGen tool")]
--- TSClientGen.Tests/Properties/AssemblyInfo.cs
+++ TSClientGen.Tests/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("9.3.0")]
-[assembly: AssemblyInformationalVersion("9.3.0")]
+[assembly: AssemblyVersion("9.3.1")]
+[assembly: AssemblyInformationalVersion("9.3.1")]
 
 [assembly: AssemblyTitle("TSClientGen.Tests")]
 [assembly: AssemblyDescription("Unit tests for TSClientGen tool")]
