fee01a42e6bc4cc291f1b4b4900dfd55c3423ad7
Nikolay Zoteev <n.zoteev@smartcat.ai>
2024-07-26 16:15:46 +0400

    DEV-1373 Changed transport contracts & api method generator for support the newest versions of axios

--- TSClientGen.Tests/Properties/AssemblyInfo.cs
+++ TSClientGen.Tests/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("9.3.1")]
-[assembly: AssemblyInformationalVersion("9.3.1")]
+[assembly: AssemblyVersion("9.3.2")]
+[assembly: AssemblyInformationalVersion("9.3.2")]
 
 [assembly: AssemblyTitle("TSClientGen.Tests")]
 [assembly: AssemblyDescription("Unit tests for TSClientGen tool")]
