82f6fa36927fd79725d89234fb74c4f0ad7b00b7
Nikita Kholodilov <n.kholodilov@smartcat.ai>
2025-02-24 15:38:32 +0500

    DEV-7530 Update all projects in PT solution from netcoreapp3.1 to net8.0

--- TSClientGen.Core/Properties/AssemblyInfo.cs
+++ TSClientGen.Core/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("10.0.2")]
-[assembly: AssemblyInformationalVersion("10.0.2")]
+[assembly: AssemblyVersion("10.1.0")]
+[assembly: AssemblyInformationalVersion("10.1.0")]
 
 [assembly: AssemblyTitle("TSClientGen.Core")]
 [assembly: AssemblyDescription("Main assembly with the code generation stuff")]
