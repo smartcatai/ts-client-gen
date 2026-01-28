2b88796fb6a6f672501684007a870f03f7e35c1c
Alexander Kozhukhov <a.kozhuhov@smartcat.ai>
2026-01-28 18:37:30 +0300

    Add net 10.0 to TFMs

--- TSClientGen.Contract/Properties/AssemblyInfo.cs
+++ TSClientGen.Contract/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("10.1.0")]
-[assembly: AssemblyInformationalVersion("10.1.0")]
+[assembly: AssemblyVersion("10.1.1")]
+[assembly: AssemblyInformationalVersion("10.1.1")]
 
 [assembly: AssemblyTitle("TSClientGen.Contract")]
 [assembly: AssemblyDescription("Contracts assembly with attributes for decorating api for TSClientGen tool")]
